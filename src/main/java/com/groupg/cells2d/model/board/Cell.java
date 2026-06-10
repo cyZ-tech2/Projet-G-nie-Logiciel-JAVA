ff1abc44388fc915d40f70716691f1f037bb0ebb
Rindra <rkt.rindra95@gmail.com>
2026-06-10 16:06:57 +0200

    gtuèi

--- src/main/java/com/groupg/cells2d/model/board/Cell.java
+++ src/main/java/com/groupg/cells2d/model/board/Cell.java
@@ -20,7 +20,10 @@ public class Cell{
     private CellState state; //A revoir car peut etre incoherent avec seirDATA
     private SEIRData seirData;
     private int row;
-    private int col;
+    private int col; 
+    private boolean insideParis;
+     private String districtId;
+    private String districtName;
 
     public Cell(String cellId, int population, int row, int col) {
         if(population < 0) {
@@ -45,7 +48,7 @@ public class Cell{
         this.col = col;
         this.row = row;
     }
-    /**
+        /**
      * Evolves the internal epidemic state of the cell
      * without considering neighboring cells.
      * @param beta transmission rate
@@ -54,10 +57,10 @@ public class Cell{
      * @param mortalityRate mortality rate
      */
     /**public void evolve(TimeStep step){
-     seirData.computeNextStep();
-     }
-     */
-    public void addCase(PatientCase c) { //Simple version : if a patient is added , the cell becomes infected
+        seirData.computeNextStep();
+    }
+    */
+    public void addCase(PatientCase c) { //Simple version : if a patient is added , the cell becomes infected 
         this.state = CellState.INFECTED;
     }
     /**
@@ -97,15 +100,15 @@ public class Cell{
     public int getRow() {
         return row;
     }
-
+    
     public void setRow (int row) {
         this.row = row;
     }
 
-    public int getCol() {
+     public int getCol() {
         return col;
     }
-
+    
     public void setCol(int col) {
         this.col = col;
     }
@@ -142,6 +145,29 @@ public class Cell{
         this.seirData = seirData;
     }
 
+    public boolean isInsideParis() {
+        return insideParis;
+    }
+
+    public void setInsideParis( boolean insideParis){
+        this.insideParis = insideParis;
+    }
+     public String getDistrictId() {
+        return districtId;
+    }
+
+    public void setDistrictId(String districtId) {
+        this.districtId = districtId;
+    }
+
+    public String getDistrictName() {
+        return districtName;
+    }
+
+    public void setDistrictName(String districtName) {
+        this.districtName = districtName;
+    }
+
     /**
      * Updates the cell state based on the infection rate.
      * @param infectionRate ratio of infected over total population
@@ -157,5 +183,5 @@ public class Cell{
             setState(CellState.CRITICAL);
         }
     }
-
 }
+
